eccc15497f6b7144e5b3a0e686836579a2236620
Marek Dworecki <marek.dworecki01@gmail.com>
2024-11-22 09:54:15 +0100

    New landing page in progress

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -43,22 +43,13 @@
                     gsap.to('.info', {opacity: 1, duration: 0.5, delay: 0.5})
                 }
             },
-            rulesInfoFunc(){
-                if(this.animState === 0){
-                    this.animState = 3
-                    gsap.to('.title', {y: -150, duration: 0.5})
-                    gsap.fromTo('.info', {opacity: 0, duration: 0.5,}, {opacity: 1, duration: 0.5, delay: 0.5,})
-                    this.parkInfo = 'Nic tu jeszcze nie ma'
-                }
-                else if(this.animState === 1 || this.animState === 2){
-                    this.animState = 3
-                    gsap.to('.info', {opacity: 0, duration: 0.5})
-                    setTimeout(function(){
-                        this.parkInfo = 'Nic tu jeszcze nie ma'
-                    }.bind(this), 500)
-                    gsap.to('.info', {opacity: 1, duration: 0.5, delay: 0.5})
-                }
-            }
+            gotoMapHover(){
+                gsap.set('.earth', {rotateY: 0})
+                gsap.set('.pin', {y: 0,})
+                gsap.to('.earth', {rotateY: 360, duration: 2, ease: "none"})
+                gsap.to('.pin', {y: -50, duration: 0.5})
+                gsap.to('.pin', {y: 0, delay: 0.5})
+            },
             
         }
     }
@@ -66,29 +57,31 @@
 
 </script>
 
-<template lang="pug"> 
-
-.main
-    .main_panel
-        .main_panel--title
-            .title Park Krajobrazowy: Dolina Jezierzycy
-            .info {{ parkInfo }} 
-        .main_panel--nav
-            ul
-                li(@click="parkInfoFunc") O parku
-                li(@click="projectInfoFunc") O projekcie
-                li(@click="rulesInfoFunc") Regulamin parku 
-            div
-                router-link(to='/map')
-                    button Przejdź do mapy
-video.backgroundVideo(autoplay muted loop)
-    source(src='../assets/PracaINZ.mp4' type='video/mp4')
+<template> 
+    <div id="main">
+        <div class="main--nav">
+            <ul>
+                <li><span>O parku</span></li>
+                <li><span>O projekcie</span></li>
+            </ul>
+        </div>
+        <div class="main--title">
+            Park Krajobrazowy<br>Dolina Jezierzycy
+        </div>
+        <div class="main--gotoMap">
+            <div @mouseover="gotoMapHover" class="button">
+                <img class="earth" src="../assets/earth.png">
+                <img class="pin" src="../assets/pin.png">
+            </div>
+        </div>
+    </div>
+    <video class="backgroundVideo" autoplay muted loop>
+        <source src="../assets/PracaINZ.mp4" type="video/mp4">
+    </video>
 </template>
 
-<style scoped lang="scss">
+<style lang="scss">
 
-    $background: rgba(79, 177, 219, 0.562);
-    
     .backgroundVideo{
         position: fixed;
         left: 0;
@@ -106,414 +99,70 @@ video.backgroundVideo(autoplay muted loop)
         }
     }
 
-    .main{
-        width: 100vw;
-        height: 100vh;
+    #main{
         margin: 0;
         padding: 0;
-        //background-image: url('../assets/landingpagebackground.jpg');
-        background-size: cover;
-        background-position: center;
-        opacity: 1;
+        width: 100vw;
+        height: 100vh;
+        background-color: #1696ffb2;
+        display: flex;
+        flex-direction: column;
+    }
+    .main--nav{
+        width: 100%;
+        height: 25%;
+        color: white;
+        font-size: 1.5vw;
 
-        &_panel{
+        ul{
             width: 100%;
             height: 100%;
-            margin: 0;
-            padding: 0;
-            background-color: $background;
             display: flex;
             flex-direction: row;
-            align-items: center;
-
-            &--title{
-                color: white;
-                width: 50%;
-                text-align: center;
-                font-size: 2.5vw;
-                text-shadow: 3px 4px 3px rgba(66, 68, 90, 1);
-                display: flex;
-                flex-direction: column;
-                height: 100%;
-
-                .title{
-                    height: 60%;
-                    padding: 1%;
-                    display: flex;
-                    align-items: end;
-                    z-index: 99;
-                }
-                .info{
-                    font-size: 1vw;
-                    padding-left: 5%;
-                    padding-right: 5%;
-                    font-family: Ojuju;
-                    letter-spacing: 0.25cap;
-                    margin: 0;
-                    height: 40%;
-                    position: relative;
-                    top: -150px;
-                    @media screen and (max-width: 1281px) {
-                        font-size: 1.2vw;
-                    }
-                    @media screen and (max-width: 1024px) {
-                        font-size: 1.3vw;
-                    }
-                }
-                
-            }
-
-            &--nav{  
-                width: 50%;
-                height: 100%;
-                display: flex;
-                text-align: center;
-                align-items: center;
-                flex-direction: column;
-                
-
-                ul{
-                    width: 100%;
-                    height: 10%;
-                    display: flex;
-                    margin: 0;
-                    padding: 0;
-                    flex-direction: row;
-                    color: white;
-                    list-style-type: none;
-                    align-items: center;
-
-                    li{
-                        width: 33.3%;
-                        font-size: 1.75vw;
-                        text-shadow: 3px 4px 3px rgba(66, 68, 90, 1);
-                        cursor: pointer;
-                        font-family: Ojuju;
-                        letter-spacing: 0.25cap;
-
-                        &:hover{
-                            text-shadow: none;
-                            transition: text-shadow 0.3s ease-in-out;
-                        }
-                    }
-                }
-
-                div{
-
-                    width: 50%;
-                    height: 90%;
-                    display: flex;
-                    align-items: center;
-
-
-                    button{
-                        font-family: Ojuju; 
-                        margin-left: auto;
-                        margin-right: auto;
-                        width: 150px;
-                        height: auto;
-                        font-size: 1.3rem;
-                        padding: 2px;
-                        background-color: transparent;
-                        color: white;
-                        border: solid 2px white;
-                        text-shadow: 3px 4px 3px rgba(66, 68, 90, 1);
-                        cursor: pointer;
-
-                        &:hover{
-                            text-shadow: none;
-                            border-color: rgba(255, 255, 255, 0.562);
-                            transition: 0.3s ease-in-out;
-                        }
-                    }
-                }
-                
-
-            }
-        }
-    }
-</style>
-<style scoped lang="scss">
-@media screen and (max-width: 768px) {
-
-    $background: rgba(79, 177, 219, 0.562);
-
-    .main{
-        width: 100vw;
-        height: 100vh;
-        margin: 0;
-        padding: 0;
-        opacity: 1;
-
-        &_panel{
-            width: 100%;
-            height: 100%;
+            list-style-type: none;
             margin: 0;
             padding: 0;
-            background-color: $background;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
+            padding-top: 1%;
 
-            &--title{
-                color: white;
-                width: 90%;
+            li{
+                width: 50%;
                 text-align: center;
-                font-size: 4vw;
-                text-shadow: 3px 4px 3px rgba(66, 68, 90, 1);
-                display: flex;
-                flex-direction: column;
-                height: 100%;
-                order: 1;
-
-                .title{
-                    height: auto;
-                    padding: 10%;
-                    display: flex;
-                    align-items: end;
-                    z-index: 99;
-                    position: relative;
-                    top: -25%;
-                }
-                .info{
-                    font-family: Ojuju;
-                    font-size: 2.2vw;
-                    letter-spacing:0.3cap;
-                    padding-left: 0%;
-                    padding-right: 0%;
-                    margin: 0;
-                    width: 100%;
-                    height: 40%;
-                    position: relative;
-                    top: -50%;
-                }
-                
-            }
-
-            &--nav{  
-                width: 90%;
-                height: 100%;
-                display: flex;
-                text-align: center;
-                align-items: center;
-                flex-direction: column;
-                
-
-                ul{
-                    width: 100%;
-                    height: 10%;
-                    display: flex;
-                    margin: 0;
-                    padding: 0;
-                    flex-direction: row;
-                    color: white;
-                    list-style-type: none;
-                    align-items: center;
-                    order: 0;
-
-                    li{
-                        width: 33.3%;
-                        font-family: Ojuju;
-                        letter-spacing: 0.25cap;
-                        font-size: 2.5vw;
-                        text-shadow: 3px 4px 3px rgba(66, 68, 90, 1);
-                        cursor: pointer;
-
-                        &:hover{
-                            text-shadow: none;
-                            transition: text-shadow 0.3s ease-in-out;
-                        }
-                    }
-                }
-
-                div{
-
-                    width: 50%;
-                    height: 90%;
-                    display: flex;
-                    align-items: center;
-                    
-
-
-                    a{
-                        display: flex;
-                        position: absolute;
-                        bottom: 75px;
-                        align-items: center;
-                        text-align: center;
-                        left: calc(50vw - 75px);
-
-                        a:-webkit-any-link{
-                            text-decoration: none;
-                        }
-
-                        button{
-                            font-family: Ojuju; 
-                            width: 150px;
-                            height: auto;
-                            font-size: 1.3rem;
-                            padding: 2px;
-                            background-color: transparent;
-                            color: white;
-                            border: solid 2px white;
-                            text-shadow: 3px 4px 3px rgba(66, 68, 90, 1);
-                            cursor: pointer;
-                        
-
-                            &:hover{
-                                text-shadow: none;
-                                border-color: rgba(255, 255, 255, 0.562);
-                                transition: 0.3s ease-in-out;
-                            }
-                    }
-                    }
-                }
-                
-
             }
         }
     }
-}
-</style>
-<style scoped lang="scss">
-@media screen and (max-width: 576px) {
-
-    $background: rgba(79, 177, 219, 0.562);
-
-    .main{
+    .main--title{
         width: 100vw;
-        height: 100vh;
-        margin: 0;
-        padding: 0;
-        opacity: 1;
+        height: 50%;
+        color: white;
+        display: flex;
+        justify-content: center;
+        text-align: center;
+        font-size: 5vw;
+        padding-top: 5%;
+    }
+    .main--gotoMap{
+        width: 100%;
+        height:25%;
 
-        &_panel{
-            width: 100%;
+        .button{
+            margin-left: auto;
+            margin-right: auto;
             height: 100%;
-            margin: 0;
-            padding: 0;
-            background-color: $background;
+            width: 256px;
             display: flex;
-            flex-direction: column;
-            align-items: center;
+            justify-content: center;
 
-            &--title{
-                color: white;
-                width: 90%;
-                text-align: center;
-                font-size: 5vw;
-                text-shadow: 3px 4px 3px rgba(66, 68, 90, 1);
-                display: flex;
-                flex-direction: column;
-                height: 100%;
-                order: 1;
-
-                .title{
-                    height: auto;
-                    padding: 10%;
-                    display: flex;
-                    align-items: end;
-                    z-index: 99;
-                    position: relative;
-                    top: -25%;
-                }
-                .info{
-                    font-family: Ojuju;
-                    font-size: 2.5vw;
-                    letter-spacing:0.3cap;
-                    padding-left: 0%;
-                    padding-right: 0%;
-                    margin: 0;
-                    width: 100%;
-                    height: 40%;
-                    position: relative;
-                    top: -50%;
-                }
+            img{
+                position: fixed;
+                width: 128px;
+                height: 128px;
+                cursor: pointer;
                 
+                &:first-child{
+                    margin-top: 3%;
+                }
             }
-
-            &--nav{  
-                width: 90%;
-                height: 100%;
-                display: flex;
-                text-align: center;
-                align-items: center;
-                flex-direction: column;
-                
-
-                ul{
-                    width: 100%;
-                    height: 10%;
-                    display: flex;
-                    margin: 0;
-                    padding: 0;
-                    flex-direction: row;
-                    color: white;
-                    list-style-type: none;
-                    align-items: center;
-                    order: 0;
-
-                    li{
-                        width: 33.3%;
-                        font-family: Ojuju;
-                        letter-spacing: 0.25cap;
-                        font-size: 3vw;
-                        text-shadow: 3px 4px 3px rgba(66, 68, 90, 1);
-                        cursor: pointer;
-
-                        &:hover{
-                            text-shadow: none;
-                            transition: text-shadow 0.3s ease-in-out;
-                        }
-                    }
-                }
-
-                div{
-
-                    width: 100%;
-                    height: 90%;
-                    display: flex;
-                    align-items: center;
-                    
-
-                    a{
-                        display: flex;
-                        position: absolute;
-                        bottom: 75px;
-                        align-items: center;
-                        text-align: center;
-                        left: calc(50vw - 75px);
-
-                        a:-webkit-any-link{
-                            text-decoration: none;
-                        }
-
-                        button{
-                            font-family: Ojuju; 
-                            width: 150px;
-                            height: auto;
-                            font-size: 1.3rem;
-                            padding: 2px;
-                            background-color: transparent;
-                            color: white;
-                            border: solid 2px white;
-                            text-shadow: 3px 4px 3px rgba(66, 68, 90, 1);
-                            cursor: pointer;
-                        
-
-                            &:hover{
-                                text-shadow: none;
-                                border-color: rgba(255, 255, 255, 0.562);
-                                transition: 0.3s ease-in-out;
-                            }
-                    }
-                    }
-                    
-                }
-                
-
-            }
+            
         }
     }
-}
 </style>
